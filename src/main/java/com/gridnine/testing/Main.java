0c482e6702b4945fdf7390448265b2e53928a5e9
alkor63 <alkor@medi-spa.ru>
2023-11-10 02:05:17 +0300

    Закончил подготовку.

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,4 +1,13 @@
 package com.gridnine.testing;
 
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Flight> flightList = FlightBuilder.createFlights();
+        for (Flight flight:flightList) {
+            System.out.println(flight);
+        }
+
+    }
 }
