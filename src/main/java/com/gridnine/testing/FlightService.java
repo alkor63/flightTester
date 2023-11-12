a28a36c36d397a035b4d323e897824398309137d
alkor63 <alkor@medi-spa.ru>
2023-11-12 22:22:03 +0300

    Закончил с предварительной валидацией списка перелётов

--- src/main/java/com/gridnine/testing/FlightService.java
+++ src/main/java/com/gridnine/testing/FlightService.java
@@ -1,54 +1,102 @@
 package com.gridnine.testing;
 
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class FlightService {
 //    сервисный класс для проверки соответствия авиарейсов нашим требованиям
 
-public static boolean departureBeforeArrival(Segment segment) {
-    if (segment.getDepartureDate().isBefore(segment.getArrivalDate())) return true;
-    else {
-        System.out.println("Некорректная информация - прибытие раньше взлёта: "+ segment + " *** Illegal departure/arrival Date");
+//    Метод для проверки того, что время взлёта предшествует времени посадки
+// true ==> всё в порядке, false ==> в сегменгте указано некорректное время
+    public static boolean departureBeforeArrival(Segment segment) {
+        if (segment.getDepartureDate().isBefore(segment.getArrivalDate())) return true;
+        else {
+            System.out.println("Некорректная информация - прибытие раньше взлёта: " + segment + " *** Illegal departure/arrival Date");
+            return false;
+        }
+    }
+
+    // Метод проверяет, что длительность полёта не превышает рекордные на сегодня 19 часов 14 минут
+    // false ==> всё в порядке, true ==> в сегменгте указано некорректное время (слишком длительный перелёт)
+    public static boolean veryLongFlight(Segment segment) {
+        long flightTimeInMinutes = ChronoUnit.MINUTES.between(segment.getDepartureDate(), segment.getArrivalDate());
+        if (flightTimeInMinutes > (19 * 60 + 14)) {
+            System.out.println("!!! слишком длительный полёт: " + segment + "  !!!");
+            System.out.println("+++ для справки: самый длительный перелёт длился 19 часов 14 минут +++");
+            return true;
+        }
         return false;
     }
-}
-public static boolean veryLongFlight(Segment segment) {
-    long flightTimeInMinutes = ChronoUnit.MINUTES.between(segment.getDepartureDate(), segment.getArrivalDate());
-    if (flightTimeInMinutes > (19*60 + 14)) {
-        System.out.println("!!! слишком длительный полёт: "+segment+"  !!!");
-        System.out.println("+++ для справки: самый длительный перелёт длился 19 часов 14 минут +++");
-        return true;
-    }
-    return false;
-}
 
-public static List<Flight> validateFlight(List<Flight> flightList) {
-    List<Flight> validFlights = new ArrayList<>();
-    for (Flight flight:flightList) {
-        List<Segment> segments = new ArrayList<>();
-        for (Segment segment:flight.getSegments()){
-            if(!veryLongFlight(segment) && departureBeforeArrival(segment))
-            {
-                segments.add(segment);
+//  Метод для проверки валидности списка перелётов
+//  Формируем новый список, в который "не пропускаем" сегменты ("рейсы")
+//  имеющие некорректное время вылета/посадки
+    public static List<Flight> validateFlight(List<Flight> flightList) {
+        List<Flight> validFlights = new ArrayList<>();
+        for (Flight flight : flightList) {
+            List<Segment> segments = new ArrayList<>();
+            for (Segment segment : flight.getSegments()) {
+                if (!veryLongFlight(segment) && departureBeforeArrival(segment)) {
+                    segments.add(segment);
+                }
+            }
+            if (segments.size() > 0) {
+                validFlights.add(new Flight(segments));
             }
         }
-if (segments.size() > 0) {
-    validFlights.add(new Flight(segments));
-}
-    }
-    return validFlights;
-}
-public static void filter1(List<Flight> flightList){
-    System.out.println("Список перелётов, доступных в данный момент:");
-
-}
-    public static void filter2(List<Flight> flightList){
-
-    }
-    public static void filter3(List<Flight> flightList){
-
+        return validFlights;
     }
 
+    //  Метод для проверки валидности списка перелётов
+//  Формируем новый список, в который "не пропускаем" сегменты ("рейсы")
+//  имеющие время посадки раньше времени вылета
+    public static List<Flight> flightsToFuture(List<Flight> flightList) {
+        List<Flight> validFlights = new ArrayList<>();
+        for (Flight flight : flightList) {
+            List<Segment> segments = new ArrayList<>();
+            for (Segment segment : flight.getSegments()) {
+                if (departureBeforeArrival(segment)) {
+                    segments.add(segment);
+                }
+            }
+            if (segments.size() > 0) {
+                validFlights.add(new Flight(segments));
+            }
+        }
+        return validFlights;
+    }
+
+//  Метод выводит в консоль список перелётов, доступных в данный момент
+    public static void departureFromNow(List<Flight> flightList) {
+        if (flightList != null) {
+            System.out.println("Список перелётов, доступных в данный момент:");
+            for (Flight flight : flightList) {
+                Segment firstSegment = flight.getSegments().get(0);
+                    if (firstSegment.getDepartureDate().isAfter(LocalDateTime.now())) {
+                        System.out.println(flight);
+                    }
+                }
+            }
+        }
+
+    //  Метод, который показывает перелеты, где общее время, проведённое на земле, не превышает два часа
+    public static void travelWithWaitingLessThen2hours(List<Flight> flightList) {
+        if (flightList != null) {
+            System.out.println("Список перелётов, где общее время, проведённое на земле, не превышает 2 часа \n !!! (минимальное время на пересадку не лимитировано) !!!");
+            for (Flight flight : flightList) {
+                int numSeg = flight.getSegments().size();
+                if (numSeg > 1) {   // перелёты, состоящие из 2 и более сегментов
+                    long waitingTime = 0;
+                    List<Segment> segments = new ArrayList<>(flight.getSegments());  //разбираем перелёт на составные сегменты
+                    for (int i = 0; i < numSeg - 1; i++) {
+                        waitingTime = waitingTime + ChronoUnit.MINUTES.between(segments.get(i).getArrivalDate(), segments.get(i + 1).getDepartureDate());
+                    }  //  определили суммарное время "на земле"
+                    if (waitingTime <= 2*60)   // те самые 2 часа
+                        System.out.println(segments);
+                }
+            }
+        }
+    }
 }
--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,19 +1,46 @@
 package com.gridnine.testing;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+//  получаем сгенерированный для нас список перелётов
         List<Flight> flightList = FlightBuilder.createFlights();
+        System.out.println(" **** тестовый набор перелётов ****");
         for (Flight flight:flightList) {
             System.out.println(flight);
+//  выводим этот список в консоль (для справки)
         }
+//
+        System.out.println("1) Исключаем из тестового набора перелёты с вылетом до текущего момента времени");
+//  Вызываем метод, который показывает все перелеты, доступные с текущего момента времени
+        FlightService.departureFromNow(flightList);
 
-    List<Flight> validFlights = FlightService.validateFlight(flightList);
-        for (Flight flight:validFlights) {
-            System.out.println(flight);
-        }
+        List<Flight> correctFlights = FlightService.flightsToFuture(flightList);
+// *** убираем из списка сегменты ("рейсы") у которых время посадки предшествует времени взлёта ***
+        System.out.println("2) убираем из списка сегменты (рейсы) у которых время посадки предшествует времени взлёта");
+        correctFlights.forEach(System.out::println);
+
+        System.out.println("3) перелеты, где общее время, проведённое на земле, не превышает два часа");
+        FlightService.travelWithWaitingLessThen2hours(flightList);
+// ---------------------------------------------------------------------------------------------------------
+        System.out.println("=================================================");
+        System.out.println("В списке есть сегмент длительностью 6 суток");
+        System.out.println("кроме того 'рейс в прошлое' тоже остался в списке ");
+        System.out.println("Предлагаю сначала провести валидацию перелётов ");
+        System.out.println("и далее Правила тестировать на корректном списке  ");
+
+        System.out.println("-------------------------------------------------");
+        System.out.println("------ Проводим валидацию списка перелётов ------");
+        List<Flight> validFlights = FlightService.validateFlight(flightList);
+// Проводим валидацию списка перелётов, в т.ч.
+// *** убираем из списка сегменты ("рейсы") у которых время посадки предшествует времени взлёта ***
+        System.out.println("Список корректных перелётов: ");
+        validFlights.forEach(System.out::println);
+//  Вызываем метод, который показывает все перелеты, доступные с текущего момента времени
+        FlightService.departureFromNow(validFlights);
+
+//  Вызываем метод, который показывает перелеты, где общее время, проведённое на земле, не превышает два часа
+        FlightService.travelWithWaitingLessThen2hours(validFlights);
     }
 }
